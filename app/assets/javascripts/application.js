a751d6f05964b140d717a880d5ab47990b78c80c
Malith <malith@revox.io>
2016-06-25 00:27:45 +0530

    Init

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require pace/pace.min
+//= require jquery
+//= require jquery_ujs
+//= require bootstrapv3/js/bootstrap.min
+//= require jquery-block-ui/jqueryblockui.min
+//= require jquery-unveil/jquery.unveil.min
+//= require jquery-scrollbar/jquery.scrollbar.min
+//= require jquery-numberAnimate/jquery.animateNumbers
+//= require jquery-validation/js/jquery.validate.min
+//= require bootstrap-select2/select2.min
+//= require js/webarch
+//= require turbolinks
+//= require_tree .
